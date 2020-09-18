c2d181648eaf9d815f3dd2f386c4a1887aaa938f
mirco <mircobaalmans@live.nl>
2020-09-18 20:12:00 +0200

    Added menus and toggle script
    Added menus and a toggle script. Also created basic UI Prefabs.

---
+++ Planet Miner/Assets/Scripts/UI/MenuToggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuToggle : MonoBehaviour
+{
+    public GameObject[] menus;
+    private GameObject currentMenu;
+
+    private void Start()
+    {
+        toggleMenu(menus[0]);
+    }
+
+    public void toggleMenu(GameObject newMenu)
+    {
+        if(currentMenu != null)
+            currentMenu.SetActive(false);
+        
+        foreach(GameObject menu in menus)
+        {
+            if (menu == newMenu)
+            {
+                newMenu.SetActive(true);
+                currentMenu = newMenu;
+            }
+        }
+    }
+}
