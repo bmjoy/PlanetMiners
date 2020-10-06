36624c961903c992a5f5b10a38dab9d854762d3f
mirco <mircobaalmans@live.nl>
2020-10-06 11:21:36 +0200

    Camera follows Unit

---
+++ Planet Miner/Assets/Scripts/Camera/CameraOnUnit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOnUnit : MonoBehaviour
+{
+    Unit unit;
+    private void Update()
+    {
+        if (unit == null)
+            unit = FindObjectOfType<Unit>();
+
+        if (unit != null)
+        {
+            Vector3 position = unit.transform.position;
+
+            position.y = 10;
+
+            transform.position = position;
+        }
+    }
+}
