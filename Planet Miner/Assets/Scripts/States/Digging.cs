e6b0722170477e10e0f133e9f2a2045959067cc6
mirco <mircobaalmans@live.nl>
2020-11-16 14:15:06 +0100

    Added digging state

---
+++ Planet Miner/Assets/Scripts/States/Digging.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Digging : State
+{
+    Rubble _rubble;
+    float _digSpeed;
+    public Digging(Rubble rubble,float digSpeed)
+    {
+        _rubble = rubble;
+        _digSpeed = digSpeed;
+    }
+
+    public override void run()
+    {
+        _rubble.doDamage(_digSpeed);
+
+        if (_rubble.health <= 0)
+        {
+            GameObject.FindObjectOfType<TerrainControl>().removeRubble(_rubble);
+
+        }
+    }
+}
