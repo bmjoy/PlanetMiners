108b8cf3d8a92d5e610ecb6c2c62bb7e1abc6e50
mirco <mircobaalmans@live.nl>
2020-11-16 12:19:49 +0100

    Changed way of finding path and ground

--- Planet Miner/Assets/Scripts/Tasks/DrillTask.cs
+++ Planet Miner/Assets/Scripts/Tasks/DrillTask.cs
@@ -1,29 +1,38 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DrillTask : Task
 {
     private Wall _targetWall;
     private Ground _groundNextToWall;
+
     public DrillTask(Wall wall)
     {
         _targetWall = wall;
-        findWallGround();
         _multipleMode = true;
     }
 
     public override void start()
     {
         if (!isNextToWall())
-            unit.changeState(new Walking(unit.transform.position, _groundNextToWall.transform.position, unit));
+        {
+            if (Pathfinding.checkForPath(unit.transform.position, _groundNextToWall.transform.position))
+                unit.changeState(new Walking(unit.transform.position, _groundNextToWall.transform.position, unit));
+            else
+                end();
+        }
+
         else
             unit.changeState(new Drilling(_targetWall, 1, unit));
     }
 
     public override void execute()
     {
+        if (taskEnded) return;
+
         if (isNextToWall() && unit.getState().GetType() != typeof(Drilling))
             unit.changeState(new Drilling(_targetWall, 1, unit));
     }
@@ -35,28 +44,35 @@ public class DrillTask : Task
 
     private void findWallGround()
     {
-        foreach (GameObject go in _targetWall.neighbours.Values)
-        {
-            if (go == null) continue;
+        GameObject nearestGround = null;
+        float minDist = float.MaxValue;
 
-            if (go.CompareTag("Ground"))
-            {
-                _groundNextToWall = go.GetComponent<Ground>();
-                break;
-            }
+        IEnumerable<GameObject> wallNeighbours =
+            from neighbour in _targetWall.neighbours.Values
+            where neighbour != null
+            select neighbour;
+
+        foreach (GameObject go in wallNeighbours)
+        {
+            if (go.TryGetComponent<Ground>(out Ground g))
+                if (Mathf.Abs(Vector3.Distance(unit.transform.position, go.transform.position)) < minDist)
+                {
+                    nearestGround = go;
+                    minDist = Mathf.Abs(Vector3.Distance(unit.transform.position, go.transform.position));
+                }
         }
-        if (_groundNextToWall == null && unit != null)
+
+        if (nearestGround != null)
+            _groundNextToWall = nearestGround.GetComponent<Ground>();
+
+
+        if (_groundNextToWall == null)
             end();
-        return;
     }
 
     private bool isNextToWall()
     {
-        if (_groundNextToWall == null)
-            findWallGround();
-
-        Debug.Log("drilltask for " + unit);
-
+        findWallGround();
         return unit.isAtPosition(_groundNextToWall.transform.position);
     }
 }
